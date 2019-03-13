b77e19b0fcd1cf8e774703f9d9e24d0261a71377
Tristan Chaumont <tristan.chaumont68@gmail.com>
2019-03-13 15:33:41 +0100

    Ajout du bouton 'Clear database'

--- app/src/main/java/si1/ccm/projet/MainActivity.java
+++ app/src/main/java/si1/ccm/projet/MainActivity.java
@@ -3,7 +3,6 @@ package si1.ccm.projet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -78,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_database) {
             Intent dbmanager = new Intent(getBaseContext(), AndroidDatabaseManager.class);
             startActivity(dbmanager);
         }
